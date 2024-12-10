9420b052ff00b84718e8d110282fdfbf1d84297b
WYXkk <2068559520@qq.com>
2024-12-10 14:13:43 +0800

    Update template.js

--- 2024/template.js
+++ 2024/template.js
@@ -1,7 +1,9 @@
-let sumReducer=(a,b)=>a+b;
+Array.prototype.sum=function(){return this.reduce((a,b)=>a+b)}
 let int=x=>parseInt(x);
+let arr=(n,x=0)=>new Array(n).fill(x);
 let dir4=[[-1,0],[1,0],[0,1],[0,-1]];
 let dir8=[[-1,0],[1,0],[0,1],[0,-1],[-1,1],[1,-1],[1,1],[-1,-1]];
+let bound=(x,y)=>x>=0&&x<n&&y>=0&&y<n;
 
 let debug=0;
 let a=(debug?exp:document.body.innerText).trim();
